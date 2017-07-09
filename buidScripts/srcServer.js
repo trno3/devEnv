53c1d6003744578ec3d3818bce0ce2999f66f433
TRoNi <plonder@gmail.com>
2017-07-09 20:38:02 +0200

    webpack testing config

--- buidScripts/srcServer.js
+++ buidScripts/srcServer.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, '../src/index.html'));
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, (err) => {
-   if (err) {
-      console.log(err);
-   } else{
-      open('http://localhost:' + port);
-   }
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
 })
