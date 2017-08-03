51c0b19e0690c9770bfdb74b3a5ff29a6c2c96cf
TRoNi <plonder@gmail.com>
2017-08-03 17:42:16 +0200

    Change output color in webpack prod build stats message

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -24,7 +24,7 @@ webpack(webpackConfig).run((err, stats) => {
         jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)));
     }
 
-    console.log(`Webpack stats: ${stats}`);
+    console.log(chalk.magenta(`Webpack stats: ${stats}`));
 
     // if we got this far, the build succeeded.
     console.log(chalk.green('Your app has been built for production and written to /dist!'));
