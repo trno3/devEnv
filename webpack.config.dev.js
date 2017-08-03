17839cda0e2c940e1f3aa41bfb072093fa928430
TRoNi <plonder@gmail.com>
2017-08-03 17:13:53 +0200

    Updated to webpack 3

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,29 +1,50 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    debug: true,
-    devtool: '#inline-source-map',
-    noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
-    target: 'web',
-    output: {
-        path: path.resolve(__dirname, 'src'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            inject: true
-        })
-    ],
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-            { test: /\.css$/, loaders: ['style', 'css'] }
+  devtool: "#inline-source-map",
+  devServer: {
+    noInfo: false
+  },
+
+  entry: [path.resolve(__dirname, "src/index")],
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      inject: true,
+      debug: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {}
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
         ]
-    }
-}
+      }
+    ]
+  }
+};
