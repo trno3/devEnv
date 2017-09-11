344b8658672c5a3bf8baaacb8ec7ce52f12b3303
TRoNi <toni_hp@hotmail.com>
2017-09-11 16:09:56 +0200

    webpack con karma

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,49 +2,49 @@ import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  devtool: "#inline-source-map",
-  devServer: {
-    noInfo: false
-  },
+   devtool: "#inline-source-map",
+   devServer: {
+      noInfo: false
+   },
 
-  entry: [path.resolve(__dirname, "src/index")],
-  target: "web",
-  output: {
-    path: path.resolve(__dirname, "src"),
-    publicPath: "/",
-    filename: "bundle.js"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      inject: true,
-      debug: true
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {}
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
+   entry: [path.resolve(__dirname, "src/index")],
+   target: "web",
+   output: {
+      path: path.resolve(__dirname, "src"),
+      publicPath: "/",
+      filename: "bundle.js"
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: "src/index.html",
+         inject: true,
+         debug: true
+      })
+   ],
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+               loader: 'babel-loader',
+               options: {}
             }
-          }
-        ]
-      }
-    ]
-  }
+         },
+         {
+            test: /\.css$/,
+            use: [
+               {
+                  loader: 'style-loader'
+               },
+               {
+                  loader: 'css-loader',
+                  options: {
+                     modules: true
+                  }
+               }
+            ]
+         }
+      ]
+   }
 };
